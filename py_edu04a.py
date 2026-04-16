fecbe8f6f49ed69cef78a9b620fe2aafe97681f9
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2026-04-16 09:31:28 +0200

    Add files via upload

---
+++ py_edu04a.py
@@ -0,0 +1,33 @@
+#--== Python ==--
+#Ćwiczenie: Sumowanie dwóch liczb.
+#
+a = float(input("Liczba A: "))              #1.
+b = float(input("Liczba B: "))              #2.
+print("Suma dwóch liczb wynosi: ", (a+b))   #3
+w = a+b                                     #4
+print("Suma dwóch liczb wynosi: ", w)       #5
+"""
+Legenda:
+1. Zadeklarowanie zmiennej "a" i przypisanie do niej wartości
+   liczbowej, podanej przez użytkownika za pomocą klawiatury.
+   Przed przypisaniem do zmiennej "a" liczby podanej przez
+   użytkownika, następuje konwersja tej liczby do typu float
+   (czyli do typu rzeczywistego).
+=
+2. Zadeklarowanie zmiennej "b" i przypisanie do niej wartości
+   liczbowej, podanej przez użytkownika za pomocą klawiatury.
+   Przed przypisaniem do zmiennej "b" liczby podanej przez
+   użytkownika, następuje konwersja tej liczby do typu float
+   (czyli do typu rzeczywistego).
+=
+3. Wyświetlenie na ekranie tekstu umieszczonego pomiędzy cudzysłowami
+   oraz wyświetlenie sumy dwóch liczby. Liczby przechowywane
+   są w zmiennych "a" i "b".
+=
+4. Obliczenie sumy dwóch liczb i przypisanie wyniku do zmiennej "w".
+   Sumowane liczby są przechowywane w zmiennych "a" i "b".
+=
+5. Wyświetlenie na ekranie tekstu umieszczonego pomiędzy
+   cudzysłowami oraz wyniku sumy dwóch liczb.
+   Wynik przechowywany jest w zmiennej "w".
+"""
