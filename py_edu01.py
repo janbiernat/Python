c3d6f915f6d90d94faffac61405a69a3e7db9b42
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2026-04-16 09:30:12 +0200

    Add files via upload

---
+++ py_edu01.py
@@ -0,0 +1,26 @@
+#--== Python ==--
+#Ćwiczenie: Zmienne.
+#
+a = 5                                                  #1
+b :int = 3                                             #2
+print("Zmienna \"a\" zawiera: ", a)                    #3
+print("Zmienne \"a\" i \"b\" zawierają: ", a, ", ", b) #4
+"""
+Legenda:
+1. Zadeklarowanie zmiennej "a" i przypisanie do niej wartości 5.
+=
+2. Zadeklarowanie zmiennej "b" liczbowej całkowitej i przypisanie
+   do niej wartości 3.
+=
+3. Wyświetlenie na ekranie:
+   3.1. Tekstu umieszczonego pomiędzy cudzysłowami.
+   3.2. Liczby przechowywanej w zmiennej "a".
+   Zapis \" umożliwia wyświetlenie na ekranie znaku cudzysłowia.
+=
+4. Wyświetlenie na ekranie:
+   4.1. Tekstu umieszczonego pomiędzy cudzysłowami.
+   4.2. Liczby przechowywanej w zmiennej "a".
+   4.3. Znaku przecinka.
+   4.4. Liczby przechowywanej w zmiennej "b".
+   Zapis \" umożliwia wyświetlenie na ekranie znaku cudzysłowia.
+"""
