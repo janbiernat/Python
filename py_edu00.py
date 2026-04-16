73a8fa576ea6a712e3cb105f20c704d30cc70f38
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2026-04-16 09:29:49 +0200

    Add files via upload

---
+++ py_edu00.py
@@ -0,0 +1,11 @@
+#--== Python ==--
+#Ćwiczenie: Wyświetlenie tekstu na ekranie.
+#
+print("Cześć! Jestem językiem programowania i nazywam się Python!") #1.
+input("Naciśnij klawisz ENTER ...")                                 #2.
+"""
+Legenda:
+1. Wyświetlenie na ekranie tekstu umieszczonego
+   pomiędzy cudzysłowami za pomocą instrukcji "print".
+2. Zatrzymanie programu do czasu naciśnięcia klawisza ENTER.
+"""
