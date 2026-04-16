b95ee840300819ce75fe3613c34abc6d02fcab25
Jan T. Biernat <46463762+janbiernat@users.noreply.github.com>
2026-04-16 09:31:04 +0200

    Add files via upload

---
+++ py_edu03.py
@@ -0,0 +1,15 @@
+#--== Python ==--
+#Ćwiczenie: Zmienne i pobieranie danych z klawiatury.
+#
+l = input("Podaj liczbę: ") #1.
+print(l)                    #2.
+print("Podano liczbę: ", l) #3.
+"""
+Legenda:
+1. Pobranie danych (w tym przypadku liczby) z klawiatury
+   i przypisanie pobranej liczby do zmiennej "l".
+2. Wyświetlenie pobranej liczby na ekranie.
+   Wyświetlana liczba jest przechowywana w zmiennej "l".
+3. Wyświetlenie na ekranie tekstu umieszczonego pomiędzy
+   cudzysłowami oraz liczby przechowywanej w zmiennej "l".
+"""
